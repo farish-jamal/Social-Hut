e68fd2cb1d85ac53a400ac46ab7bfbb9a1d36a09
farish-jamal <farishjamal98@gmail.com>
2022-09-24 12:26:08 +0530

    Sidebar Done!

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,10 +1,35 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import Follow from './follow/Follow'
 import "./Profile.css"
 
 function Profile() {
   return (
     <div className='profile'>
-        <h1>I am Profile</h1>
+        <div className="profile_top">
+          <img src='https://picsum.photos/200/60?grayscale' alt='bg' />
+          <Avatar className='profile_img' style={{width: "50px", height: "50px"}}/>
+          <p className="profile_name">Farish Jamal</p>
+          <p className="profile_username">@farishjamal</p>
+          <div className="profile_follower">
+            <div className="followers">
+              <p className="number">7654</p>
+              <p className="text">Followers</p>
+            </div>
+            <div className="followers">
+              <p className="number">7654</p>
+              <p className="text">Followings</p>
+            </div>
+          </div>
+            <div className="button">My Profile</div>
+        </div>
+        <div className="profile_bottom">
+          <p className="top">Who is to follow you</p>
+          <Follow name="Aman Ali" username="ripoff"/>
+          <Follow name=" Md Ehtesham" username="sam123"/>
+          <Follow name="Faisal Eqbal" username="shutterthelens"/>
+          <p className="bottom_button">Show More</p>
+        </div>
     </div>
   )
 }
---
+++ src/components/profile/follow/Follow.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Avatar } from '@mui/material'
+import "./Follow.css"
+
+function Follow({name, username}) {
+  return (
+    <div className="bottom_follow">
+        <Avatar style={{width: "40px", height: "40px"}}/>
+        <div className="bottom_name">
+            <p className="name_name">{name}</p>
+            <p className="name_username">@{username}</p>
+        </div>
+        <button>Follow</button>
+    </div>
+  )
+}
+
+export default Follow
